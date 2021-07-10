831532ee172bfd58449dbb260aa8ab9efbd16800
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2021-07-09 20:17:45 -0500

    fix interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  // Get references and values
+  const principal = document.getElementById("principal").value;
+  const rate = document.getElementById("rate").value;
+  const years = document.getElementById("years").value;
+  const result = document.getElementById("result");
+  // Compute interest
+  const interest = (principal * years * rate) / 100;
+  // Get years
+  const year = new Date().getFullYear() + parseInt(years);
+  // Validate principal input
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return;
+  }
+  // Update result HTML
+  result.innerHTML = `If you deposit <mark>${principal}</mark>, <br /> at an interest rate of <mark>${rate}%</mark>. <br /> You will receive an amount of <mark>${interest}</mark>, <br /> in the year <mark>${year}</mark>`;
 }
-        
+
+function updateRate() {
+  const rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
+}
+
+const rateSlider = document.getElementById("rate");
+
+// Listen for change event in the slider
+rateSlider.addEventListener("change", updateRate);
